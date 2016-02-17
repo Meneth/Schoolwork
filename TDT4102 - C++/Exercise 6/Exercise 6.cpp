9cc8ff15ad183ce637bd4609cac41da4aaf9efe1
Meneth <github@meneth.com>
2016-02-17 19:09:22 +0100

    Finished exercise 6

--- TDT4102 - C++/Exercise 6/Exercise 6.cpp
+++ TDT4102 - C++/Exercise 6/Exercise 6.cpp
@@ -5,8 +5,41 @@
 
 using namespace std;
 
+void solveEquations() {
+	float a, b, c, d, p, q;
+	cout << "ax + by = p" << endl;
+	cout << "Enter a: ";
+	cin >> a;
+	cout << "Enter b: ";
+	cin >> b;
+	cout << "Enter p: ";
+	cin >> p;
+
+	cout << "cx + dy = q" << endl;
+	cout << "Enter c: ";
+	cin >> c;
+	cout << "Enter d: ";
+	cin >> d;
+	cout << "Enter q: ";
+	cin >> q;
+
+	Matrix2x2 matrix = Matrix2x2(a, b, c, d);
+	Vector2 vector = Vector2(p, q);
+
+	try {
+		Vector2 answer = matrix.inverse() * vector;
+		cout << "x = " << answer.get(0) << endl;
+		cout << "y = " << answer.get(1) << endl;
+	}
+	catch (std::invalid_argument e) {
+		cout << "No solution possible." << endl;
+	}
+	
+}
+
 int main()
 {
+	/*
 	Matrix2x2 matrix = Matrix2x2();
 	cout << matrix << endl;
 
@@ -31,5 +64,7 @@ int main()
 	d *= b;
 	d = d - a + c;
 	cout << d << endl;
+	*/
+	solveEquations();
     return 0;
 }
